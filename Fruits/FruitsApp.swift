31e61e96667359b99802634eecec3cd67a8208ac
abdWasiq <abdul.wasiq@naheed.pk>
2022-04-19 15:08:19 +0500

    project created

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by NaheedPK on 19/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
